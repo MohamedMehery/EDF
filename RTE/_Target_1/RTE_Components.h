274406a0717e7daa6bdb13bb4fdd8c911e0d1ce1
MohamedMehery <49691904+MohamedMehery@users.noreply.github.com>
2023-01-09 21:51:36 +0200

    dev

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'edf' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "TM4C123.h"
+
+/*  ARM::CMSIS:RTOS:Keil RTX:4.82.0 */
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+/*  Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.5.1 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+
+
+#endif /* RTE_COMPONENTS_H */
